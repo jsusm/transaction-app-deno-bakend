e9e8fe358eda3d008db730f74a2b2cc368f2efa2
Jesus Marcano <jsusmarcano.jm@gmail.com>
2023-03-23 12:43:03 -0400

    Add redis configuration environment variables

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,7 +1,8 @@
 import { redis as Redis } from "../deps.ts";
 
 export const redis = await Redis.connect({
-  hostname: "127.0.0.1",
-  port: 6379,
-  password: "randompassword",
+  hostname: Deno.env.get("REDISHOST") ?? '',
+  port: Deno.env.get("REDISPORT"),
+  password: Deno.env.get("REDISPASSWORD"),
+  username: Deno.env.get("REDISUSER"),
 });
