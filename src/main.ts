d1aadf6084e05930c3134acc8cc2dfa75aed444e
Jesus Marcano <jsusmarcano.jm@gmail.com>
2023-03-24 13:09:55 -0400

    Use PORT environment variable

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "https://deno.land/std@0.177.0/dotenv/load.ts";
 import { bootstrapApp } from './app.ts'
 
 const app = bootstrapApp()
+const PORT = parseInt(Deno.env.get('PORT') || '8080')
 
-console.log("App listen on port 8080");
-app.listen({ port: 8080 });
+console.log("App listen on port: " + PORT);
+app.listen({ port: PORT });
